0aaa29e273e42f85ac6b4ddafd93c353d25ead5e
thenoob100pi <gouthamsrinivas93@gmail.com>
2017-12-16 13:42:55 +0530

    Applicative order vs normal order of method evaluation

---
+++ Basics 1/1.3_Substitution model For Procedure evaluation/applicative_order vs normal_order.lisp
@@ -0,0 +1,32 @@
+(define (square x)  (* x x))
+(define (sumOfSquares x y) (+ (square x) (square y)))
+(define (f a) (sumOfSquares (+ a 1) (* a 2)))
+(display (f 2))  ; displays 25
+
+#| 
+ Applicative Order (Evaluate the arguments and then apply)
+
+First evaluate the operands and operator
+
+( f 2) 
+(sumOfSquares (+a 1)(*a 2)       
+(sumOfSquares (+ 2 1) (* 2 2))   ;evaluate now and then apply to the function
+(sumOfSquares 3 4) 
+(+ (square 3) (+square 4))
+(+ (* 3 3) (* 4 4)
+(+ 9 16) ==> 25
+
+--------------------------------------------------------------------------------
+
+ Normal Order  ( Fully expand and then finally reduce) 
+
+( f 2) 
+(sumOfSquares (+a 1)(*a 2)        
+(sumOfSquares (+ 2 1) (* 2 2))       ;Keep expanding .Dont evaluate till the end
+(+ (square (+ 2 1) (+square (* 2 2)) ;Keep expanding .Dont evaluate till the end
+(+ (* (+ 2 1) (+ 2 1)) (*  (* 2 2) (* 2 2)))  ; Fully expanded
+(+ (* 3 3) (* 4 4))
+(+ 9 16)
+25
+
+|#
