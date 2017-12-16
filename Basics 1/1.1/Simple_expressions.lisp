8354c73dd9b84443e40da4a50237fc17a5b791b8
thenoob100pi <gouthamsrinivas93@gmail.com>
2017-12-16 13:12:34 +0530

    scheme 1.1 with basic expressions

---
+++ Basics 1/1.1/Simple_expressions.lisp
@@ -0,0 +1,17 @@
+#|
+Lisp(Scheme) uses prefix notation 
+Example : (+ 3 3) --> 6
+          (* 4 5) --> 20
+
+|#
+
+(display (+ 5 4)) ;Evaluates the expression and displays the result
+(display "\n")    ;For newline
+
+#|
+defining variables in Scheme
+Define keyword to define or name things|#
+
+
+(define x 2)
+(display x)       ; prints 2
